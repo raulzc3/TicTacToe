da51502b126ed12fad99499c7432c28f695b4c9e
Raúl González <raulzc3pc@gmail.com>
2021-11-16 23:08:38 +0100

    Added current player indicator

--- js/figures.js
+++ js/figures.js
@@ -1,18 +1,18 @@
 "use strict";
 
-function circle() {
+function circle(classList) {
   const circle = document.createElement("div");
-  circle.classList.add("circle", "figure");
+  circle.classList.add("circle", classList || "figure");
   return circle;
 }
 
-function cross() {
+function cross(classList) {
   const stick = document.createElement("div");
   const innerStick = document.createElement("div");
   const cross = document.createElement("div");
   stick.classList.add("stick", "parent");
   innerStick.classList.add("stick", "child");
-  cross.classList.add("cross", "figure");
+  cross.classList.add("cross", classList || "figure");
 
   stick.append(innerStick);
   cross.append(stick);
