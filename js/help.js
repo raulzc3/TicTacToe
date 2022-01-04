e5bd94a33d3a7e099aa15713557beec93dcc68f7
Raúl González <raulzc3pc@gmail.com>
2022-01-04 20:33:24 +0100

    Show help on startup toggle added and minor fixes

--- js/help.js
+++ js/help.js
@@ -2,17 +2,30 @@ const help = document.querySelector(".help");
 const overlay = document.querySelector("#help");
 const openHelpButton = document.querySelector(".helpButton");
 const closeHelpButton = document.querySelector(".dismissHelp");
+const hideCompetitiveCheck = document.querySelector("#hideCompetitiveCheck");
+const storage = window.localStorage;
+const hideCompetitive =
+  storage.getItem("hideCompetitive") === "true" ? true : false;
 
-//Todo: Optimize class-based animation and avoid timeout
+//Show help if storage is false or has no data in it
+if (!hideCompetitive) overlay.classList.remove("hidden");
+
+//Set checkbox value according to user decision
+hideCompetitiveCheck.checked = hideCompetitive;
 
 closeHelpButton.onclick = () => {
+  //Save user decision in storage
+  storage.setItem("hideCompetitive", hideCompetitiveCheck.checked);
+  //Close help modal
   help.classList.add("delete");
   overlay.classList.add("fading");
   setTimeout(() => {
+    //Todo: Optimize class-based animation and avoid timeout
     overlay.classList.add("hidden");
   }, 300);
 };
 
+//Show help modal
 openHelpButton.onclick = () => {
   overlay.classList.remove("fading", "hidden");
   help.classList.remove("delete");
